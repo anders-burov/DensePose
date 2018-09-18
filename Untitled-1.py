1f69adf1f39f48a879ab0b3c436148e22cdd969f
davidleejy <davidleejy@gmail.com>
2018-09-18 21:38:29 +0800

    Update. Created two margin cosine embedding loss.

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,42 +2,99 @@ import msmt17_v1_utils
 import torch
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import pairwise_distances
+import sklearn
+print(sklearn.__file__)
 
-def soft_cosine_embedding_loss(input1, input2, target, pos_margin=0.0, neg_margin=0.0):
-    # Computes cosine loss with soft margins for positive and negative samples.
-    # 
-    # Example of soft margin:
-    #     pos_margin = 0.1
-    #     Loss contributed by a positive pair is 0 if cosine distance is greater than 0.9 (0.9 == 1 - 0.1).
-    #
-    # Motivation:
-    #     cosine distance has range [-1, +1].
-    #     similar vectors should be closer to +1.    (note: target 1)
-    #     orthogonal vectors should give 0.
-    #     dissimilar vectors should be closer to -1. (note: target 0)
-    #     But, many a times, dissimilar/similar vectors aren't perfectly alike.
-    #     For example, the same person's attire appearing slightly differently
-    #     under different pose and/or illumination conditions - in such a 
-    #     case, vectors representing him, v1 & v2, should be close to +1 but
-    #     it may not make sense to "force" dist(v1, v2) to be exactly +1.
-    #     Dissimilar vectors, v3 & v4, by the same logic, also should not be
-    #     forced to be exactly -1. There could a be another dissimilar vector, v5,
-    #     that is closer to, say, v3 conceptually. It is better to have dist(v5,v3) < dist(v5,v4)
-    #     than "force" dist(v5,v3) == dist(v5,v4) == -1.
-    #
-    # Arguments:
-    #     input1 & input2 are shaped (n_samples, dimensions)
-    #     target is shaped (n_samples,). Either 1 or 0. 1 is positive/similar pair. 0 is negative/dissimilar pair.
-    D = torch.nn.functional.cosine_similarity(input1, input2)
-    zeroes = torch.zeros(target.shape[0])
-    D_pos = 1.0 - pos_margin - D
-    D_neg = D - neg_margin
-    Z_D_pos = torch.stack([zeroes, D_pos], dim=1)
-    Z_D_neg = torch.stack([zeroes, D_neg], dim=1)
-    loss = (1.0*y) * torch.max(Z_D_pos, dim=1)[0]  +  (1.0 - y) * torch.max(Z_D_neg, dim=1)[0]
-    loss = torch.sum(loss) / (1.0 * target.shape[0])
-    return loss
+train_loss_record= [99, 34, 0.8]
 
+fig = plt.figure()
+ax = fig.gca()
+plt.title('train & val loss')
+np.arange(1, len(train_loss_record))
+plt.plot(range(1, len(train_loss_record)+1), train_loss_record, 'b')
+plt.ylabel('loss'); plt.xlabel('epochs')
+plt.show()
+# plt.plot(list(range(1, len(train_loss_record+1))), train_loss_record, 'b')
+
+exit()
+
+
+
+
+
+def scores(embeds1, embeds2, metric):
+    # embeds1 and embeds2 are numpy arrays of shape (n_samples, n_features)
+    # where embeds1[i,:] and embeds2[i,:] represent embeddings from
+    # the SAME person.
+    if metric == 'cosine':
+        D = pairwise_distances(embeds1, embeds2, metric='cosine')
+        D = (D - 1.0) * -1.0  # Compute similarity from distances.
+        non_diag_coords = np.where(~np.eye(D.shape[0],dtype=bool))
+        imposter_scores = D[non_diag_coords]
+        genuine_scores = np.diagonal(D)
+        return D, genuine_scores, imposter_scores  # Returns a distance matrix ("scipy convention"), genuine scores, imposter scores.
+    else:
+        raise NotImplementedError
+
+
+def plot_scores(genuines, imposters, title, n_bins=100):
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.gca()
+    plt.title(title)
+    ax.hist(genuines, bins=n_bins, alpha = 0.5)
+    ax.hist(imposters, bins=int(n_bins*1.2), color='r', alpha = 0.3)
+    return fig, plt
+
+
+
+# geniune and imposter distances:
+
+torch.set_printoptions(precision=10)
+
+i1s = torch.randn(10, 2) #* torch.randint(2, 100, (10,2))
+i2s = torch.randn(10, 2) #* torch.randint(2, 100, (10,2))
+D = torch.nn.functional.cosine_similarity(i1s, i2s)
+print('D', D)
+D2 = pairwise_distances(i1s.numpy(), i2s.numpy(), metric='cosine')
+D2 = (D2 - 1.0) * -1.0  # Compute similarity from distances.
+non_diag_coords = np.where(~np.eye(D2.shape[0],dtype=bool))
+imposter_scores = D2[non_diag_coords]
+print('imopster')
+print(len(imposter_scores))
+print(imposter_scores)
+genuine_scores = np.diagonal(D2)
+diag = genuine_scores
+print('D2 diag', diag)
+print('D2 imposter distances:')
+print(D2[0,1], torch.nn.functional.cosine_similarity(i1s[[0],:], i2s[[1],:]))
+print(D2[0,2], torch.nn.functional.cosine_similarity(i1s[[0],:], i2s[[2],:]))
+print(D2[0,3], torch.nn.functional.cosine_similarity(i1s[[0],:], i2s[[3],:]))
+
+
+
+dist_mat, geniune_scores, imposter_scores = scores(i1s, i2s, 'cosine')
+fig, plt = plot_scores(geniune_scores, imposter_scores, 'tile123')
+fig.savefig('delme.jpg')
+
+
+
+# plt.show();
+# plt.show(block=False)
+# plt.draw(); 
+# import time; time.sleep(100)
+# print('asdf')
+
+exit()
+
+# im
+
+# T1 = torch.randn(24,224,224,3) 
+# T1 = T1.view(T1.shape[0] * T1.shape[-1], 224, 224)
+# print T1.shape
+
+# exit()
 
 i1 = torch.randn(10, 2)
 i2 = torch.randn(10, 2)
--- example-basic-train.py
+++ example-basic-train.py
@@ -48,6 +48,7 @@ def two_margin_cosine_embedding_loss(input1, input2, target, pos_margin=0.0, neg
     loss = torch.sum(loss) / (1.0 * target.shape[0])
     return loss
 
+
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
@@ -107,6 +108,29 @@ class ContrastiveLoss(torch.nn.Module):
         else:
             raise Exception('No such option.')
         
+
+def scores(embeds1, embeds2, metric):
+    # embeds1 and embeds2 are numpy arrays of shape (n_samples, n_features)
+    # where embeds1[i,:] and embeds2[i,:] represent embeddings from
+    # the SAME person.
+    if metric == 'cosine':
+        D = pairwise_distances(embeds1, embeds2, metric='cosine')
+        D = (D - 1.0) * -1.0  # Compute similarity from distances.
+        non_diag_coords = np.where(~np.eye(D.shape[0],dtype=bool))
+        imposter_scores = D[non_diag_coords]
+        genuine_scores = np.diagonal(D)
+        return D, genuine_scores, imposter_scores  # Returns a distance matrix ("scipy convention"), genuine scores, imposter scores.
+    else:
+        raise NotImplementedError
+
+
+def plot_scores(genuines, imposters, title, n_bins=100):
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.title(title)
+    ax.hist(genuines, bins=n_bins, alpha = 0.5)
+    ax.hist(imposters, bins=int(n_bins*1.2), color='r', alpha = 0.3)
+    return fig, plt
 
 
 # def train(args, model, device, train_loader, optimizer, epoch):
@@ -222,6 +246,8 @@ import math
 import random
 import torch.optim as optim
 import os
+from sklearn.metrics.pairwise import pairwise_distances
+import matplotlib.pyplot as plt
 from IUV_stack_utils import *  #TODO
 
 
@@ -252,6 +278,15 @@ def combined_IUVstack_from_multiple_chips(dataload, pid, chip_paths, trainortest
     return combine_IUV_stacks(IUV_stack_list=indiv_stacks, mode=combine_mode)
 
 
+def preprocess_IUV_stack(IUV_stack, device):
+    # IUV_stack: Format is format of output of create_IUVstack(image_file, IUV_png_file).
+    #            This code can be MODIFIED to also take in a HSV or HS (no V) type of IUVstack.
+    # device: torch device. E.g. 'cuda' or 'cpu'.
+    S = IUV_stack[:, 16:256-16, 16:256-16, :]    # Crop to 24x224x224x3
+    S = normalize_to_reals_0to1(S)               # Normalize
+    S = torch.Tensor(S)                          # Convert to torch tensors
+    S = S.to(device)
+    return S.view(24*3, 224, 224)
 
 
 if __name__ == '__main__':
@@ -262,29 +297,42 @@ if __name__ == '__main__':
     overfit = True
     epochs = 100
     batch_size = 3 #TODO 32  # take it as number of identities in a mini-batch. If 8 identities, there'll be 16 (8 x 2) pairs - 8 same pairs, 8 diff pairs. E.g. Jane-jane pair (same) , Jane-Tom pair (diff), Ben-Ben pair (same), Ben-Aaron pair (diff), ...
-    
+    plot_training_metric = True
+    plot_loss = True
+
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     dataload = msmt17_v1_utils.MSMT17_V1_Load_Data_Utils(images_train_dir='/data/MSMT17_V1/train', 
                                                             images_test_dir='/data/MSMT17_V1/test', 
                                                             denseposeoutput_train_dir='/data/IUV-densepose/MSMT17_V1/train', 
                                                             denseposeoutput_test_dir='/data/IUV-densepose/MSMT17_V1/test')
-    net = resnet_custom.resnet18(input_channels=24*3)
+    net = resnet_custom.resnet18(input_channels=24*3, num_classes=256)
     net = Siamese_Net(net)
     net = net.to(device)  # yes! must re-assign
-    contrastive_loss = ContrastiveLoss(option='cosine')
+    contrastive_loss = ContrastiveLoss(option='two margin cosine', pos_margin=0.1, neg_margin=0.2)
+    distance_type = 'cosine'
+    # contrastive_loss = ContrastiveLoss(option='cosine')
+    # distance_type = 'cosine'
+    # contrastive_loss = ContrastiveLoss(option='euclidean')
+    # distance_type = 'euclidean'
     # optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
-    optimizer = optim.Adam(net.parameters(), amsgrad=True) #defaults: lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0   https://pytorch.org/docs/stable/optim.html?highlight=optim#torch.optim.Adam
+    optimizer = optim.Adam(net.parameters(), amsgrad=True, lr=0.0001) #defaults: lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0   https://pytorch.org/docs/stable/optim.html?highlight=optim#torch.optim.Adam
+
+    train_loss_record = []
+    val_loss_record = []
+
     for epoch in range(epochs):
         if not overfit:
             pids_shuffled = np.random.permutation(dataload.train_persons_cnt)
         else:
-            print('OVERFIT!' * 100)
-            pids_shuffled = np.array([0, 1, 2])
+            print('OVERFIT!' * 20)
+            pids_shuffled = np.array([4, 8, 13])
             batch_size = 3
         #print(pids_shuffled.size)
         n_batches = int(math.ceil( pids_shuffled.size / (batch_size*1.0) ))
         # print(n_batches)
+        train_loss_batches = []
+
         for bidx in range(n_batches):
             batch_pids = pids_shuffled.take(np.arange(batch_size) + batch_size * bidx, mode='wrap')
             batch_of_IUV_stacks = []
@@ -297,35 +345,42 @@ if __name__ == '__main__':
                 split2 = chips[int(len(chips)/2) : ]
                 S1 = combined_IUVstack_from_multiple_chips(dataload, pid=pidstr, chip_paths=split1, trainortest='train', combine_mode='average v2')
                 S2 = combined_IUVstack_from_multiple_chips(dataload, pid=pidstr, chip_paths=split2, trainortest='train', combine_mode='average v2')
-                #
-                # TODO compute intersect. COuld be here or below.
-                #
-                S1 = S1[:, 16:256-16, 16:256-16, :] # Crop to 24x224x224x3
-                S2 = S2[:, 16:256-16, 16:256-16, :] 
-                S1 = normalize_to_reals_0to1(S1)    # Normalize
-                S2 = normalize_to_reals_0to1(S2)
-                S1 = torch.Tensor(S1); S2 = torch.Tensor(S2) # Convert to torch tensors
-                S1 = S1.to(device); S2 = S2.to(device)
-                S1 = S1.view(24*3, 224, 224); S2 = S2.view(24*3, 224, 224)
-                batch_of_IUV_stacks.append( (S1, S2) )
+                
+                # S1 = preprocess_IUV_stack(S1, device)
+                # S2 = preprocess_IUV_stack(S2, device)
+                # S1 = S1[:, 16:256-16, 16:256-16, :] # Crop to 24x224x224x3
+                # S2 = S2[:, 16:256-16, 16:256-16, :] 
+                # S1 = normalize_to_reals_0to1(S1)    # Normalize
+                # S2 = normalize_to_reals_0to1(S2)
+                # S1 = torch.Tensor(S1); S2 = torch.Tensor(S2) # Convert to torch tensors
+                # S1 = S1.to(device); S2 = S2.to(device)
+                # S1 = S1.view(24*3, 224, 224); S2 = S2.view(24*3, 224, 224)
+                batch_of_IUV_stacks.append( (S1.copy(), S2.copy()) )
             #print('Done compute IUV stacks')
             input1s = []; input2s = []; targets = []
             for person in range(len(batch_of_IUV_stacks)):
-                #
-                # TODO compute intersect. COuld be here or above.
-                #
-                # Same person pair:
-                input1s.append(batch_of_IUV_stacks[person][0])
-                input2s.append(batch_of_IUV_stacks[person][1])
-                targets.append(1)
-                # Diff persons pair:
+                ######## Same person pair ########
+                S1 = batch_of_IUV_stacks[person][0]
+                S2 = batch_of_IUV_stacks[person][1]
+                # mask = get_intersection(S1, S2)          # intersect
+                # S1 = apply_mask_to_IUVstack(S1, mask)
+                # S2 = apply_mask_to_IUVstack(S2, mask)
+                S1 = preprocess_IUV_stack(S1, device)
+                S2 = preprocess_IUV_stack(S2, device)
+                input1s.append(S1); input2s.append(S2); targets.append(1)
+                ######## Diff persons pair ########
                 persons = list(range(len(batch_of_IUV_stacks)))
                 persons.remove(person)
                 another_person = random.choice(persons)
-                assert(person != another_person) # TODO remove.
-                input1s.append(batch_of_IUV_stacks[person][random.randint(0,1)])
-                input2s.append(batch_of_IUV_stacks[another_person][random.randint(0,1)])
-                targets.append(0)
+                assert(person != another_person) # prevent edge cases like 1 person only in batch.
+                S1 = batch_of_IUV_stacks[person][random.randint(0,1)]
+                S2 = batch_of_IUV_stacks[another_person][random.randint(0,1)]
+                # mask = get_intersection(S1, S2)          # intersect
+                # S1 = apply_mask_to_IUVstack(S1, mask)
+                # S2 = apply_mask_to_IUVstack(S2, mask)
+                S1 = preprocess_IUV_stack(S1, device)
+                S2 = preprocess_IUV_stack(S2, device)
+                input1s.append(S1); input2s.append(S2); targets.append(0)
             assert(len(input1s) > 0)
             assert( len(input1s) == len(input2s) and len(input2s) == len(targets) )
             #print('len ', len(input1s))
@@ -344,9 +399,31 @@ if __name__ == '__main__':
             loss = contrastive_loss(output1s, output2s, targets)
             loss.backward()
             optimizer.step()
+
+            train_loss_batches.append(loss.detach().numpy())
+
+            if plot_training_metric:
+                assert(output1s.shape[0] % 2.0 == 0)
+                print(output1s[:3,:].shape)#s; exit()
+                n_embeds = int(output1s.shape[0] / 2.0)
+                _, geniune_scores, imposter_scores = scores(output1s[:n_embeds,:].detach().numpy(), output2s[:n_embeds,:].detach().numpy(), distance_type)
+                fig, plt = plot_scores(geniune_scores, imposter_scores, 'Scores')
+                fig.savefig('tr-scores.jpg')
+                plt.close(fig)
             
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, (bidx+1) * batch_size, pids_shuffled.size,
                 100. * (bidx+1) / n_batches, loss.item()))
+        
+        train_loss_record.append( np.sum(train_loss_batches) / (1.0 * len(train_loss_batches)) )
+        if plot_loss:
+            fig = plt.figure()
+            ax = fig.gca()
+            plt.title('train & val loss')
+            plt.plot(range(1, len(train_loss_record) + 1), train_loss_record, 'b')
+            plt.ylabel('loss'); plt.xlabel('epochs')
+            fig.savefig('loss.jpg')
+            plt.close(fig)
+        
         # for k epochs, run 1 validation.
         # save
